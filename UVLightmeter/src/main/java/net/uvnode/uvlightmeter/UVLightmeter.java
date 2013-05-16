983a9736d754937945f553bd44d50b7d934256bb
jcornwellshiel <jcornwellshiel@gmail.com>
2013-05-15 22:29:06 -0400

    Version 1.0-SNAPSHOT Now functional!

--- UVLightmeter/src/main/java/net/uvnode/uvlightmeter/UVLightmeter.java
+++ UVLightmeter/src/main/java/net/uvnode/uvlightmeter/UVLightmeter.java
@@ -13,6 +13,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
@@ -21,28 +22,55 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- *
+ * Portable light meter plugin
  * @author James Cornwell-Shiel
  */
 public class UVLightmeter extends JavaPlugin implements Listener {
     private ShapelessRecipe lightMeterRecipe;
+    private ItemStack lightMeterItemStack;
+    private ItemMeta lightMeterMetaData;
     
     private String _itemName = "Portable Light Meter";
     private List<String> _itemLore = new ArrayList<String>();
     
+    /**
+     * Fire up the plugin
+     */
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
-        createCraftingRecipe();
-        _itemLore.add(ChatColor.GOLD + "Right click a block to detect its light level.");
-        _itemLore.add(ChatColor.GOLD + "0 is pitch black, 15 is fully lit.");
+        _itemLore.add("Right click a block to detect its light level.");
+        _itemLore.add("0 is pitch black, 15 is fully lit.");
+        
+        lightMeterItemStack = new ItemStack(Material.WOOD_SWORD, 1);
+        lightMeterMetaData = lightMeterItemStack.getItemMeta();
+        lightMeterMetaData.setDisplayName(_itemName);
+        lightMeterMetaData.setLore(_itemLore);
+        
+        lightMeterItemStack.setItemMeta(lightMeterMetaData);
+        
+        lightMeterRecipe = new ShapelessRecipe(lightMeterItemStack);
+        lightMeterRecipe.addIngredient(Material.DAYLIGHT_DETECTOR).addIngredient(Material.STICK).addIngredient(Material.REDSTONE_COMPARATOR);
+        getServer().addRecipe(lightMeterRecipe);
     }
     
+    /**
+     * Shut down the plugin
+     */
     @Override
     public void onDisable() {
-        
+        // remove recipes on disable
     }
     
+    /**
+     * Respond to commands
+     * 
+     * @param sender Player/console
+     * @param cmd command
+     * @param label command name
+     * @param args command arguments
+     * @return true if we processed the command, false if not.
+     */
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("lightmeter")) {
@@ -60,43 +88,47 @@ public class UVLightmeter extends JavaPlugin implements Listener {
         } else {
             return false;
         }
-    }
-
-    private void createCraftingRecipe() {
-        ItemStack lightMeterOutput = new ItemStack(Material.WATCH, 1);
-        lightMeterOutput.setDurability((short) 0);
-        
-        ItemMeta metadata = lightMeterOutput.getItemMeta();
-        metadata.setDisplayName(_itemName);        
-        metadata.setLore(_itemLore);
-        
-        lightMeterOutput.setItemMeta(metadata);
-        
-        lightMeterRecipe = new ShapelessRecipe(lightMeterOutput);
-        lightMeterRecipe.addIngredient(Material.DAYLIGHT_DETECTOR).addIngredient(Material.STICK).addIngredient(Material.REDSTONE_COMPARATOR);
-        getServer().addRecipe(lightMeterRecipe);
-    }
+    }    
     
-    
+    /**
+     * Listens for players to craft an item, 
+     * checks if it's a light meter, and if so, 
+     * checks permissions and alerts the player if he lacks permissions.
+     * @param event
+     */
     @EventHandler
     public void onCraftItemEvent(CraftItemEvent event) {
-        if (event.getRecipe().equals(lightMeterRecipe) && !event.getWhoClicked().hasPermission("uvlightmeter.craft")) {
-            ((Player)event.getWhoClicked()).sendMessage(ChatColor.RED + "You don't have permission to craft a " + _itemName);
-            event.setCancelled(true);
+        if (event.getRecipe().getResult().equals(lightMeterItemStack)) {
+            if (event.getWhoClicked().hasPermission("uvlightmeter.craft")) {
+            } else {
+                ((Player)event.getWhoClicked()).sendMessage(ChatColor.RED + "You don't have permission to craft a " + _itemName);
+                event.setCancelled(true);
+            }
         }
     }
     
+    /**
+     * Listens for players to interact with a block with a light meter,
+     * checks permissions, and processes the reading.
+     * @param event
+     */
     @EventHandler
     public void onPlayerInteractEvent(PlayerInteractEvent event) {
-        if(event.getItem().hasItemMeta() &&
+        if(     event.getAction().equals(Action.RIGHT_CLICK_BLOCK) &&
+                event.hasItem() &&
+                event.hasBlock() &&
+                event.getItem().hasItemMeta() &&
+                event.getItem().getItemMeta().hasDisplayName() &&
                 event.getItem().getItemMeta().getDisplayName().equals(_itemName) &&
+                event.getItem().getItemMeta().hasLore() && 
                 event.getItem().getItemMeta().getLore().equals(_itemLore) &&
-                event.getPlayer().hasPermission("uvlightmeter.use") &&
-                event.hasBlock()) {
-            int lightLevel = event.getClickedBlock().getLightLevel();
-            String lightLevelText = (String) ((lightLevel > 7)?ChatColor.GREEN:ChatColor.RED + "" + lightLevel);
-            event.getPlayer().sendMessage(String.format("The light level is %s", lightLevelText));
-            event.getItem().setDurability((short) (event.getItem().getDurability()+10));
+                event.getPlayer().hasPermission("uvlightmeter.use")) {
+            int lightLevel = event.getClickedBlock().getLocation().add(0, 1, 0).getBlock().getLightLevel();
+            event.getPlayer().sendMessage(String.format("The light level is %d.", lightLevel));
+            event.getItem().setDurability((short) (event.getItem().getDurability()+1));
+            if(event.getItem().getDurability() >= (short)100) {
+                event.getPlayer().setItemInHand(null);
+            }
             event.setCancelled(true);
         }
     }
